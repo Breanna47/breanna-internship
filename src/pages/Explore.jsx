dedae194a08cac1baf980d2ffe8bdb30d2cfd0d9
Breanna Watson <breanna.watson@yahoo.com>
2026-08-31 11:15:01 -0400

    Improve author and explore loading states

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -44,6 +44,33 @@ function Countdown({ expiryDate }) {
   );
 }
 
+function ExploreSkeletonCard({ index }) {
+  return (
+    <div className="explore-card" key={index}>
+      <div className="nft__item explore-skeleton-card">
+        <div className="author_list_pp">
+          <div className="explore-skeleton-avatar"></div>
+        </div>
+
+        <div className="explore-skeleton-countdown"></div>
+
+        <div className="nft__item_wrap">
+          <div className="explore-skeleton-image"></div>
+        </div>
+
+        <div className="nft__item_info">
+          <div className="explore-skeleton-title"></div>
+
+          <div className="explore-skeleton-bottom">
+            <div className="explore-skeleton-price"></div>
+            <div className="explore-skeleton-likes"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const ExploreItems = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -63,7 +90,6 @@ const ExploreItems = () => {
         "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore",
       )
       .then((response) => {
-        console.log("Explore API:", response.data);
         setItems(response.data);
       })
       .catch((error) => {
@@ -82,15 +108,21 @@ const ExploreItems = () => {
     const sorted = [...items];
 
     if (sortBy === "price_low_to_high") {
-      sorted.sort((a, b) => Number(a.price) - Number(b.price));
+      sorted.sort(
+        (a, b) => Number(a.price) - Number(b.price),
+      );
     }
 
     if (sortBy === "price_high_to_low") {
-      sorted.sort((a, b) => Number(b.price) - Number(a.price));
+      sorted.sort(
+        (a, b) => Number(b.price) - Number(a.price),
+      );
     }
 
     if (sortBy === "likes_high_to_low") {
-      sorted.sort((a, b) => Number(b.likes) - Number(a.likes));
+      sorted.sort(
+        (a, b) => Number(b.likes) - Number(a.likes),
+      );
     }
 
     return sorted;
@@ -104,42 +136,38 @@ const ExploreItems = () => {
     }, 0);
   };
 
-  if (loading) {
-    return (
-      <div className="loading-container">
-        <div className="loading-spinner">
-          {Array.from({ length: 8 }).map((_, index) => (
-            <span
-              key={index}
-              className="loading-dot"
-              style={{ "--i": index }}
-            ></span>
-          ))}
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
-      <section className="explore-banner" data-aos="fade-up">
+      <section
+        className="explore-banner"
+        data-aos="fade-up"
+      >
         <div className="explore-banner-bubbles"></div>
         <h1>Explore</h1>
       </section>
 
-      <div className="explore-filter" data-aos="fade-up">
+      <div
+        className="explore-filter"
+        data-aos="fade-up"
+      >
         <select
           id="filter-items"
           value={sortBy}
-          onChange={(event) => setSortBy(event.target.value)}
+          onChange={(event) =>
+            setSortBy(event.target.value)
+          }
+          disabled={loading}
         >
           <option value="">Default</option>
+
           <option value="price_low_to_high">
             Price, Low to High
           </option>
+
           <option value="price_high_to_low">
             Price, High to Low
           </option>
+
           <option value="likes_high_to_low">
             Most liked
           </option>
@@ -147,107 +175,123 @@ const ExploreItems = () => {
       </div>
 
       <div className="explore-grid">
-        {sortedItems
-          .slice(0, visibleItems)
-          .map((item) => (
-            <div
-              key={item.id}
-              className="explore-card"
-              data-aos="fade-up"
-            >
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to={`/author/${item.authorId}`}
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                  >
-                    <img
-                      className="lazy"
-                      src={item.authorImage}
-                      alt="Author"
+        {loading
+          ? Array.from({ length: 8 }).map((_, index) => (
+              <ExploreSkeletonCard
+                key={index}
+                index={index}
+              />
+            ))
+          : sortedItems
+              .slice(0, visibleItems)
+              .map((item) => (
+                <div
+                  key={item.id}
+                  className="explore-card"
+                  data-aos="fade-up"
+                >
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link
+                        to={`/author/${item.authorId}`}
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                      >
+                        <img
+                          className="lazy"
+                          src={item.authorImage}
+                          alt="Author"
+                        />
+
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+
+                    <Countdown
+                      expiryDate={item.expiryDate}
                     />
 
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button type="button">
+                            Buy Now
+                          </button>
 
-                <Countdown expiryDate={item.expiryDate} />
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button type="button">Buy Now</button>
+                            <a
+                              href="/"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
 
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
+                            <a
+                              href="/"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
 
-                        <a
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
+                            <a href="/">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
 
-                        <a
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
+                      <Link
+                        to={`/item-details/${item.nftId}`}
+                      >
+                        <img
+                          src={item.nftImage}
+                          className="lazy nft__item_preview"
+                          alt={item.title}
+                        />
+                      </Link>
+                    </div>
 
-                        <a href="/">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+                    <div className="nft__item_info">
+                      <Link
+                        to={`/item-details/${item.nftId}`}
+                      >
+                        <h4>{item.title}</h4>
+                      </Link>
+
+                      <div className="nft__item_price">
+                        {item.price} ETH
+                      </div>
+
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{item.likes}</span>
                       </div>
                     </div>
                   </div>
-
-                  <Link to={`/item-details/${item.nftId}`}>
-                    <img
-                      src={item.nftImage}
-                      className="lazy nft__item_preview"
-                      alt={item.title}
-                    />
-                  </Link>
                 </div>
-
-                <div className="nft__item_info">
-                  <Link to={`/item-details/${item.nftId}`}>
-                    <h4>{item.title}</h4>
-                  </Link>
-
-                  <div className="nft__item_price">
-                    {item.price} ETH
-                  </div>
-
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{item.likes}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
       </div>
 
-      {visibleItems < sortedItems.length && (
-        <div
-          className="explore-load-more"
-          data-aos="fade-up"
-        >
-          <button
-            id="loadmore"
-            className="btn-main lead"
-            type="button"
-            onClick={handleLoadMore}
+      {!loading &&
+        visibleItems < sortedItems.length && (
+          <div
+            className="explore-load-more"
+            data-aos="fade-up"
           >
-            Load more
-          </button>
-        </div>
-      )}
+            <button
+              id="loadmore"
+              className="btn-main lead"
+              type="button"
+              onClick={handleLoadMore}
+            >
+              Load more
+            </button>
+          </div>
+        )}
     </>
   );
 };
